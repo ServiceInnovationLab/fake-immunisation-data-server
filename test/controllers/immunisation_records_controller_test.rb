cd971db11526169b81902e87731dd9ebe1da7360
Merrin <merrin.macleod@gmail.com>
2019-09-10 15:42:15 +1200

    fix test

--- test/controllers/immunisation_records_controller_test.rb
+++ test/controllers/immunisation_records_controller_test.rb
@@ -7,34 +7,8 @@ class ImmunisationRecordsControllerTest < ActionDispatch::IntegrationTest
     @immunisation_record = immunisation_records(:one)
   end
 
-  test 'should get index' do
-    get immunisation_records_url, as: :json
-    assert_response :success
-  end
-
-  test 'should create immunisation_record' do
-    assert_difference('ImmunisationRecord.count') do
-      post immunisation_records_url, params: { immunisation_record: { data: @immunisation_record.data, feijoa_id: @immunisation_record.feijoa_id } }, as: :json
-    end
-
-    assert_response 201
-  end
-
-  test 'should show immunisation_record' do
-    get immunisation_record_url(@immunisation_record), as: :json
-    assert_response :success
-  end
-
-  test 'should update immunisation_record' do
-    patch immunisation_record_url(@immunisation_record), params: { immunisation_record: { data: @immunisation_record.data, feijoa_id: @immunisation_record.feijoa_id } }, as: :json
+  test 'should get immunisation_record' do
+    get immunisation_records_url(feijoa_id: @immunisation_record.feijoa_id, date_of_birth: @immunisation_record.date_of_birth)
     assert_response 200
-  end
-
-  test 'should destroy immunisation_record' do
-    assert_difference('ImmunisationRecord.count', -1) do
-      delete immunisation_record_url(@immunisation_record), as: :json
-    end
-
-    assert_response 204
   end
 end
